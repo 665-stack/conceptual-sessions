ba757f7450f821952f4c9d4addd73d0b9265924a
unknown <mahmodulshahin665@gmail.com>
2022-03-06 19:28:10 +0600

    add more file

---
+++ dom/to-do-list.js
@@ -0,0 +1,10 @@
+document.getElementById('add-btn').addEventListener('click', function () {
+    const inputValue = document.getElementById('input-value').value;
+
+    if (inputValue == "") {
+        alert('Please enter a value')
+    }
+    else {
+
+    }
+})
